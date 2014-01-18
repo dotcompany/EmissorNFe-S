ceee19931c4cbe3c0a1c94de6d6d60ef15ab32ae
Marcos Ribeiro <marcosribeiro@dotocmpany.com.br>
2014-01-18 19:26:22 -0200

    upload componente...

---
+++ Bin/Sistemas.dpr
@@ -0,0 +1,30 @@
+program Sistemas;
+
+uses
+  Forms,
+  UFPrincipal in '..\Src\UFPrincipal.pas' {FPrincipal},
+  UModulo in '..\Src\UModulo.pas' {Modulo: TDataModule},
+  UFuncoes in '..\Src\UFuncoes.pas',
+  UFNFSEletronica in '..\Src\UFNFSEletronica.pas' {FNFSEletronica},
+  UFStatus in '..\Src\UFStatus.pas' {FStatus},
+  UGeradorChaveNFe in '..\Src\UGeradorChaveNFe.pas',
+  UFNFSCancelamento in '..\Src\UFNFSCancelamento.pas' {FNFSCancelamento},
+  UFormInutilizaNew in '..\Src\UFormInutilizaNew.pas' {FormInutilizaNew},
+  UFtpBackup in '..\Src\UFtpBackup.pas',
+  UXMLBackup in '..\Src\UXMLBackup.pas',
+  uVerificaUpdate in '..\Src\uVerificaUpdate.pas',
+  UFCartaCorrecao in '..\Src\UFCartaCorrecao.pas' {frmCartaCorrecao};
+ // VMXCCePrint in '..\Src\VMXCCePrint.pas';
+
+{$R *.res}
+
+Begin
+  Application.Initialize;
+
+  Application.Title := 'NFe';
+  Application.CreateForm(TFPrincipal, FPrincipal);
+  Application.Run;
+End.
+
+
+
